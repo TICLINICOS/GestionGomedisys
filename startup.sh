8d379e65630634626eab7751061984e1389be2bd
SebastianSuarezClinicos <sasuarez@clinicos.com.co>
2024-08-15 09:45:20 -0500

    Cambios s.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,10 @@
 python3 -m venv CargueMasivoGomedisys
 source CargueMasivoGomedisys/bin/activate
 pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
+
+#gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+
 #python3 -m venv entorno
 #source entorno/bin/activate
 #pip install -r requirements.txt

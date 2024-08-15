ca3b3729d4bd4ae5b951ae2b10e06ce4ddf1d13f
SebastianSuarezClinicos <sasuarez@clinicos.com.co>
2024-08-15 11:44:24 -0500

    cambios sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #archivo autoejecutado por el app service en azure
 #cd /home/site/wwwroot
-python3 -m venv CargueMasivoGomedisys
-source CargueMasivoGomedisys/bin/activate
-pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
+#python3 -m venv CargueMasivoGomedisys
+#source CargueMasivoGomedisys/bin/activate
+#pip install -r requirements.txt
+#gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
 
 #gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
 
@@ -11,3 +11,12 @@ gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
 #source entorno/bin/activate
 #pip install -r requirements.txt
 #gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+
+
+#!/bin/bash
+which python3
+python3 --version
+python3 -m venv CargueMasivoGomedisys
+. CargueMasivoGomedisys/bin/activate
+pip install -r requirements.txt
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
